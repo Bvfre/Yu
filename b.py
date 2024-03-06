e2f5b81b9fcaf6daa44b1585502891fe9190b0c9
Bvfre <162143566+Bvfre@users.noreply.github.com>
2024-03-06 20:02:42 +0700

    Delete b.py

--- b.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-service = Service(ChromeDriverManager().install())
